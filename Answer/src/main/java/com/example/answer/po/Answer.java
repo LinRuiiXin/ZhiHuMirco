e08534d6f4ea63251c4b8984ca82e207b4e10d86
LinRuiiXin <1303058293@qq.com>
2020-10-26 19:41:38 +0800

    迁移-Answer-不知道为什么的红色文件名

---
+++ Answer/src/main/java/com/example/answer/po/Answer.java
@@ -0,0 +1,10 @@
+package com.example.answer.po;
+
+/**
+ * TODO
+ *
+ * @author LinRuiXin
+ * @date 2020/10/26 7:39 下午
+ */
+public class Answer {
+}
--- User/src/main/java/com/example/user/UserApplication.java
+++ User/src/main/java/com/example/user/UserApplication.java
@@ -7,12 +7,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan(basePackages = "com.example.user.dao",annotationClass = Repository.class)
-@SpringCloudApplication
+@EnableFeignClients
+@SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
